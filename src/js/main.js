f94d21ef8b4256397c9039bbb93db3a3b5224366
kolialike <kolialike@gmail.com>
2016-04-19 15:17:53 +0300

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
 jQuery(function($){
-  $('.flexslider').flexslider({
-    animation: "slide"
-  });
+  //script-stiky
+	var $window = $(window);
+	var $bar = $('.bar'); 
+	$bar.each(function(index, el) {
+		var thisBar = $(this).wrap('<div class="followWrap" />');
+		var thisBarTop = thisBar.offset().top;
+		$window.on('scroll', function() {
+			var windowTop = $window.scrollTop();
+			if(windowTop >= thisBarTop){
+				thisBar.addClass("bar-fixed");
+			}else{
+				thisBar.removeClass("bar-fixed");
+			}
+		});
+	});
 });
--- src/vendor/vendor.js
+++ src/vendor/vendor.js
@@ -1,2 +1,1 @@
 //= jquery-1.11.3.min.js
-//= jquery.flexslider.js
